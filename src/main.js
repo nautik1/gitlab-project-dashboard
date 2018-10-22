33aa28f545aba37d4d8507bd106276dc041e9e7b
Nicolas P <nicolas@n-p.fr>
2018-10-22 22:43:42 +0200

    Added form validation using vee-validate

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import GitLabAPI from 'vue-gitlab-api'
 import App from './App.vue'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import VeeValidate from 'vee-validate'
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
 Vue.use(GitLabAPI)
+Vue.use(VeeValidate)
 
 new Vue({
   render: h => h(App)
